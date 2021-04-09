e6a87a93db0f89fd086efdd7f6f93250e8e3cc8d
VJavandCSharp <82200050+VJavandCSharp@users.noreply.github.com>
2021-04-09 14:36:49 +0800

    Create Script1.js

---
+++ Script1.js
@@ -0,0 +1,6 @@
+var a = window.prompt("Please enter a number: ");
+var difA = Number(a);
+var b = window.prompt("Please enter another number: ");
+var difB = Number(b);
+var sum = difA + difB;
+document.getElementById("demoJS").innerHTML = "Sum is: " + sum;
